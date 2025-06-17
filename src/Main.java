0cfd7b2ca3c4a2a73b0c94edf3596a0be585fde8
numme <nummer14@naver.com>
2025-06-17 13:13:55 +0900

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
-@FunctionalInterface
-interface MathOperation {
-    int operate(int a, int b);
-}
 public class Main {
     public static void main(String[] args) {
+        String strNum = "123a";
+
+        try {
+            int num = convertToInt(strNum);
+
+            System.out.println("변환 결과: " + num);
+        } catch (NumberFormatException e) {
+            System.out.println("숫자 형식 오류!");
+        }
+    }
+
+    public static int convertToInt(String s) throws NumberFormatException {
+        return Integer.parseInt(s);
     }
 }
